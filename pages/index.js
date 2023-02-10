a214bf4302f2e1ce848925118a88c15e09133a26
cherryjiang27 <123491476+cherryjiang27@users.noreply.github.com>
2023-02-10 17:14:40 -0500

    truffle

--- pages/index.js
+++ pages/index.js
@@ -70,11 +70,11 @@ export default function Home() {
 
                   <div className="marquee">
                     <div className="marquee__group">
-                      <img src="../truffle.jpeg" width="100px" />
-                      <img src="../truffle.jpeg" width="100px" />
-                      <img src="../truffle.jpeg" width="100px" />
-                      <img src="../truffle.jpeg" width="100px" />
-                      <img src="../truffle.jpeg" width="100px" />
+                      <img src="../truffle.png" width="100px" />
+                      <img src="../truffle.png" width="100px" />
+                      <img src="../truffle.png" width="100px" />
+                      <img src="../truffle.png" width="100px" />
+                      <img src="../truffle.png" width="100px" />
                     </div>
 
                     <div aria-hidden="true" class="marquee__group">
