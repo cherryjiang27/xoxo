b415d28048daa7916950a196c5e21eba0f1fbed0
cherryjiang27 <123491476+cherryjiang27@users.noreply.github.com>
2023-02-12 20:15:41 -0500

    k

--- pages/testcomet.js
+++ pages/testcomet.js
@@ -17,3 +17,21 @@ const TestComet = () => {
 };
 
 export default TestComet;
+
+{
+  "id": "6c2715377717",
+  "name": "my test collection",
+  "symbol": "TEST",
+  "type": "milky_way_nft",
+  "chainType": "solana",
+  "chainId": 101,
+  "subtype": "numbered_pass",
+  "description": "whats good",
+  "infiniteSupply": false,
+  "maxSupply": 100,
+  "price": 10,
+  "pricingModel": "pay_once",
+  "deployed": false,
+  "isCometToken": true,
+  "backgroundUpload": "OxnAlTCm9se8sMlnu0CeExmawJ0YVFGd4WcVgUCs3ZCfPbNm7NT9ewIfNKRfCgj7"
+}
